b4e45f7758230350abfb6791f1d977b11841fa87
Rodolfo Melo <rodolfogameroficial@gmail.com>
2025-10-28 09:50:54 -0300

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -9,29 +9,28 @@
 
 */
 
-const apiKey = "1b84cb354ab84c24b33bd83c68442eb0"  // Chave Api
-const baseUrl = "https://platform.fatsecret.com/rest/" // URL Base
+const apiKey = "be704345cdf14da4af50348a88eceb19"  // Chave Api
+const baseUrl = "https://api.spoonacular.com/recipes/complexSearch" // URL Base
 const alimentosPesquisa = document.getElementById("pesquisar")
-
-// Método para conectar a API
-const options = {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/json",
-        "Header": `Authorization: Bearer ${apiKey}`,
-        "Parameters": "method=food.get.v5&food_id=33691&format=json",
-        "Acess-Control-Allow-Origin" : "*"
-    }
-}
 
 // Função de conectar API
 async function conectarApi() {
-    let retorno = await fetch(`${baseUrl}food/v5?`,options)
-        .then(response => {
-            if(response.ok) {
-                response.json().then((json) =>{
-                    console.log(json)
-                })
-            }
-        })
+    
+    // Obtem os valores digitados pelo usuário
+    const query = alimentosPesquisa.value 
+
+    // URL completa para conexão com a API
+    const completeUrl = `${baseUrl}?apiKey=${apiKey}&query=${query}&number=5`
+
+
+    let retorno = await fetch(completeUrl)
+    .then(response => {response.json()})
+    
+    .then(data => {
+        console.log(data.results)
+    })
+    .catch(error => {
+        console.log(error)
+    })
+
 }
